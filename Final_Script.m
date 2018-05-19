af1bfe6dc8ff4e3294778fdc322b26e1c076c081
Kevin Phillips <kwp8cd@outlook.com>
2018-05-19 02:53:47 -0500

    run final script to view results

---
+++ Final_Script.m
@@ -0,0 +1,229 @@
+%main program
+%Kevin Phillips
+
+clear all, close all, clc
+
+%%
+%load the corrupted data
+
+load('ProjectMLKnew.mat');
+x = MLK_HighNoise;
+y = MLK_LowNoise;
+
+%%
+%analyze the audio
+
+spechighnoise = create_spectrum(Fs,x);
+speclownoise = create_spectrum(Fs,y);
+
+%plot High
+figure();
+subplot(2,1,1);
+plot(spechighnoise(:,1),abs(spechighnoise(:,2)));
+title('Magnitude sampled at 44.1 kHz, 16-bit resolution');
+xlabel('f'),ylabel('|Y(\omega)|');
+xlim([-Fs/2 Fs/2]);
+subplot(2,1,2);
+plot(spechighnoise(:,1),unwrap(angle(spechighnoise(:,2))));
+title('Phase sampled at 44.1 kHz, 16-bit resolution');
+xlabel('f'),ylabel('\angleY(\omega) (radians)') 
+xlim([-Fs/2 Fs/2]);
+
+%plot Low
+figure();
+subplot(2,1,1);
+plot(speclownoise(:,1),abs(speclownoise(:,2)));
+title('Magnitude sampled at 44.1 kHz, 16-bit resolution');
+xlabel('f'),ylabel('|Y(\omega)|');
+xlim([-Fs/2 Fs/2]);
+subplot(2,1,2);
+plot(speclownoise(:,1),unwrap(angle(speclownoise(:,2))));
+title('Phase sampled at 44.1 kHz, 16-bit resolution');
+xlabel('f'),ylabel('\angleY(\omega) (radians)') 
+xlim([-Fs/2 Fs/2]);
+
+%%
+%create butterworth IIR SOS filter and filter the corrupted sound file
+
+%BUTTERWORTH IIR
+name = 'Butterworth';
+%find the order of the butterwoth filter
+%specify parameters
+%cutoff freq of 2750 Hz normalized
+Wp = 2750/(Fs/2);
+%stopband 3000 Hz normalized
+Ws = 3000/(Fs/2);
+%passband ripple dB
+Rp = .1;
+%stopband attentuation dB
+Rs = 100;
+
+%find the butter order in normalized frequency and the cutoff
+[n,Wn] = buttord(Wp,Ws,Rp,Rs);
+%calculate butterworth zeros, poles, and gain k
+[z,p,k] = butter(n,Wn);
+%calculate the second order section respesentation of the filter
+sos = zp2sos(z,p,k);
+
+%filter the data with the second order section
+sosfilter_plot_spectrum(sos,x,Fs,name,'High');
+sosfilter_plot_spectrum(sos,y,Fs,name,'Low');
+
+%%
+%create Kaiser FIR impulse window and filter the corrupted sound file
+
+%KAISER
+%ex 7.6.1
+name = 'Kaiser';
+%ripple of 1/100e3 same as parks mclellan and butterworth design
+%ripple is same for pass and stopband for Kaiser window
+delta = 1/100e3;
+%normalized edge of passband
+wp = 2750*pi/(Fs/2);
+%normalized beginnihdng of stopband
+ws = 3000*pi/(Fs/2);
+%find symmetric cutoff, average of normalized frequencies
+wc = (wp+ws)/2;
+%find the width of the transition band
+deltaw = ws-wp;
+%find linear gain A
+A = -20*log10(delta);
+%find beta B 
+if A > 50
+    beta = .1102*(A-8.7);
+elseif A >= 21 && A <=50
+    beta = .5842*(A-21)^0.4+.07886*(A-21);
+elseif A < 21
+    beta = 0;
+end
+%find length parameter M, find length len, find alpha 
+M = ceil((A-8)/(2.285*deltaw));
+len = M+1;
+
+%create kaiser window and apply it to the desired impulse response to form
+%the filter
+%create x axis, of samples
+n=(0:M).';
+%create kaiser coefficients wn
+wn = kaiser(len,beta);
+%create ideal impulse response
+hdn = wc/pi*sinc((n-M/2)*wc/pi);
+%multiply the desired impulse response by the filter coefficients
+hd = hdn.*wn;
+
+%filter the data with the kaiser filter and plot
+filter_plot_spectrum(hd,1,x,Fs,name,'High');
+filter_plot_spectrum(hd,1,y,Fs,name,'Low');
+
+%%
+%create Parks-McClellan equiripple FIR filter and filter the corrupted sound
+
+%PARKS
+name = 'Parks-McClellan';
+%Passband ripple
+rp = .1;
+%Stopband ripple
+rs = 100;
+%Cutoff frequencies, [pass stop]
+f = [2750 3000].';
+%Desired amplitudes [pass stop]
+a = [1 0].';
+%change ripples from db to gain
+dev = [(10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)].';
+
+%input frequency vector, amplitude vector, deviation, and sampling
+%frequency
+%output order, normalized frequency band edges, frequency band amplitudes,
+%and weights
+[n,fo,ao,w] = firpmord(f,a,dev,Fs);
+%input order, normalized frequency band edges, frequency band amplitudes,
+%and weights
+%output transfer function numerator b (denominator is 1 for FIR's)
+b = firpm(n,fo,ao,w);
+
+%filter the data with the parks filter and plot
+filter_plot_spectrum(b,1,x,Fs,name,'High');
+filter_plot_spectrum(b,1,y,Fs,name,'Low');
+
+%%
+%Plot the filtered spectra of the sound file
+
+%PLOT
+
+%filter the data
+filbutthighnoise = sosfilt(sos,x);
+filbuttlownoise = sosfilt(sos,y);
+%filter the data
+filkaiserhighnoise = filter(hd,1,x);
+filkaiserlownoise = filter(hd,1,y);
+%filter the data
+filparkshighnoise = filter(b,1,x);
+filparkslownoise = filter(b,1,y);
+
+%create the spectra
+spechighnoisebutt = create_spectrum(Fs,filkaiserhighnoise);
+speclownoisebutt = create_spectrum(Fs,filkaiserlownoise);
+%create the spectra
+spechighnoisekais = create_spectrum(Fs,filkaiserhighnoise);
+speclownoisekais = create_spectrum(Fs,filkaiserlownoise);
+%create the spectra
+spechighnoiseparks = create_spectrum(Fs,filkaiserhighnoise);
+speclownoiseparks = create_spectrum(Fs,filkaiserlownoise);
+
+
+%plot High
+figure();
+subplot(2,1,1);
+hold on;
+plot(spechighnoisebutt(:,1),abs(spechighnoisebutt(:,2)),'r');
+plot(spechighnoisekais(:,1),abs(spechighnoisekais(:,2)),'g--');
+plot(spechighnoiseparks(:,1),abs(spechighnoiseparks(:,2)),'b:');
+title('Magnitude High Noise');
+xlabel('f'),ylabel('|Y(\omega)|');
+xlim([-5e3 5e3]);
+legend('Butterworth','Kaiser','Parks');
+hold off;
+subplot(2,1,2);
+hold on;
+plot(spechighnoisebutt(:,1),unwrap(angle(spechighnoisebutt(:,2))),'r');
+plot(spechighnoisekais(:,1),unwrap(angle(spechighnoisekais(:,2))),'g--');
+plot(spechighnoiseparks(:,1),unwrap(angle(spechighnoiseparks(:,2))),'b:');
+title('Phase High Noise');
+xlabel('f'),ylabel('\angleY(\omega) (radians)') 
+xlim([-5e3 5e3]);
+legend('Butterworth','Kaiser','Parks');
+hold off;
+
+
+%plot Low
+figure();
+subplot(2,1,1);
+hold on;
+plot(speclownoisebutt(:,1),abs(speclownoisebutt(:,2)),'r');
+plot(speclownoisekais(:,1),abs(speclownoisekais(:,2)),'g--');
+plot(spechighnoiseparks(:,1),abs(spechighnoiseparks(:,2)),'b:');
+title('Magnitude Low Noise');
+xlabel('f'),ylabel('|Y(\omega)|');
+xlim([-5e3 5e3]);
+legend('Butterworth','Kaiser','Parks');
+hold off;
+subplot(2,1,2);
+hold on;
+plot(speclownoisebutt(:,1),unwrap(angle(speclownoisebutt(:,2))),'r');
+plot(speclownoisekais(:,1),unwrap(angle(speclownoisekais(:,2))),'g--');
+plot(speclownoiseparks(:,1),unwrap(angle(speclownoiseparks(:,2))),'b:');
+title('Phase Low Noise');
+xlabel('f'),ylabel('\angleY(\omega) (radians)') 
+xlim([-5e3 5e3]);
+legend('Butterworth','Kaiser','Parks');
+hold off;
+
+%%
+%play the sound
+
+sound(MLK_HighNoise,Fs)
+pause(12)
+clear sound
+sound(filkaiserhighnoise,Fs)
+pause(12)
+clear sound
---
+++ filter_plot_spectrum.m
@@ -0,0 +1,27 @@
+function [fill] = filter_plot_spectrum( b,a,x,Fs,name,noise )
+%filters data and plots the filtered spectrum, takes transfer function
+%coefficients, b & a, data x, and sampling frequency Fs
+%returns the filtered data to the user
+
+%This program plots the filtered spectrum
+
+%filter the data
+fill = filter(b,a,x);
+
+%create the spectra
+spec = create_spectrum(Fs,fill);
+
+%plot
+figure();
+subplot(2,1,1);
+plot(spec(:,1),abs(spec(:,2)));
+title([name,', Magnitude, ',noise,'-Noise']);
+xlabel('f'),ylabel('|Y(\omega)|');
+xlim([-Fs/2 Fs/2]);
+subplot(2,1,2);
+plot(spec(:,1),unwrap(angle(spec(:,2))));
+title([name,', Phase, ',noise,'-Noise']);
+xlabel('f'),ylabel('\angleY(\omega) (radians)') 
+xlim([-Fs/2 Fs/2]);
+
+end
